087fd713b68f9b5654cf102be58d7d249b141038
EnderFall <wendywendi2019@gmail.com>
2026-04-16 10:01:37 +0700

    44

---
+++ voting (3).sql
@@ -0,0 +1,265 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 16, 2026 at 05:00 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `voting`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `candidate`
+--
+
+CREATE TABLE `candidate` (
+  `candidateid` int(11) NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `studentid` int(11) NOT NULL,
+  `vision` text NOT NULL,
+  `mission` text NOT NULL,
+  `periodid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `candidate`
+--
+
+INSERT INTO `candidate` (`candidateid`, `picture`, `studentid`, `vision`, `mission`, `periodid`) VALUES
+(2, '', 2, '1. Abc\n2. Bcd', '1. Cba\n2.Dcb', 3),
+(3, 'uploads/candidate_1776153846_8649dbba.jpg', 3, 'asa', 'asa', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+CREATE TABLE `class` (
+  `classid` int(11) NOT NULL,
+  `classname` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`classid`, `classname`) VALUES
+(1, 'X AKL'),
+(2, 'X BDP'),
+(3, 'X RPL'),
+(4, 'XI AKL'),
+(5, 'XI BDP'),
+(6, 'XI RPL');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `level`
+--
+
+CREATE TABLE `level` (
+  `levelid` int(11) NOT NULL,
+  `levelname` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `level`
+--
+
+INSERT INTO `level` (`levelid`, `levelname`) VALUES
+(1, 'Admin'),
+(2, 'Student');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `studentid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `classid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`studentid`, `name`, `classid`, `userid`) VALUES
+(2, 'A', 1, 4),
+(3, 'B', 1, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userid` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `levelid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userid`, `username`, `password`, `levelid`) VALUES
+(1, 'admin', '$2y$12$O9wso5ya598BsysOG51izu/VqO0rWAsufnCm1cU3WBHmD322yn5Wy', 1),
+(2, 'siswa', '$2y$12$SPcMjgF3wQVTyWMTEm4Vtu2IM6ypf8JIOZEKh/RfJky1QcHDsuDju', 2),
+(4, 'a', '$2y$10$EEP9VP9GSIrOaXqKKGZF7OEvCzpPW.C/IeTS5kzeuB/UxKWSIPGAG', 2),
+(5, 'b', '$2y$10$7B8SqtjZYwylIsT2lacNHOyNQGOsTvVR3ke0MXCFSO.i0pJnXF8aS', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vote`
+--
+
+CREATE TABLE `vote` (
+  `voteid` int(11) NOT NULL,
+  `studentid` int(11) NOT NULL,
+  `candidateid` int(11) NOT NULL,
+  `periodid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `voting_period`
+--
+
+CREATE TABLE `voting_period` (
+  `periodid` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `startdate` date NOT NULL,
+  `enddate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `voting_period`
+--
+
+INSERT INTO `voting_period` (`periodid`, `title`, `startdate`, `enddate`) VALUES
+(2, '2023/2024', '2023-08-23', '2023-09-01'),
+(3, 'asd', '2026-04-09', '2026-04-30');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `candidate`
+--
+ALTER TABLE `candidate`
+  ADD PRIMARY KEY (`candidateid`),
+  ADD UNIQUE KEY `uniq_candidate_student_period` (`studentid`,`periodid`);
+
+--
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`classid`);
+
+--
+-- Indexes for table `level`
+--
+ALTER TABLE `level`
+  ADD PRIMARY KEY (`levelid`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`studentid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Indexes for table `vote`
+--
+ALTER TABLE `vote`
+  ADD PRIMARY KEY (`voteid`),
+  ADD UNIQUE KEY `studentid` (`studentid`,`periodid`),
+  ADD UNIQUE KEY `uniq_vote_student_period` (`studentid`,`periodid`);
+
+--
+-- Indexes for table `voting_period`
+--
+ALTER TABLE `voting_period`
+  ADD PRIMARY KEY (`periodid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `candidate`
+--
+ALTER TABLE `candidate`
+  MODIFY `candidateid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `class`
+--
+ALTER TABLE `class`
+  MODIFY `classid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `level`
+--
+ALTER TABLE `level`
+  MODIFY `levelid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `studentid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `vote`
+--
+ALTER TABLE `vote`
+  MODIFY `voteid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `voting_period`
+--
+ALTER TABLE `voting_period`
+  MODIFY `periodid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
